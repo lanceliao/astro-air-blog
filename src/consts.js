67b7653ecba92cac3a429254dfe89220e787b1f3
Lance Liao <llshu2003@gmail.com>
2023-03-17 22:09:25 +0800

    Make footer items to be configurable via const.js

--- src/consts.js
+++ src/consts.js
@@ -3,3 +3,40 @@ export const SITE_DESCRIPTION = 'Austin Site Description';
 export const SITE_EMAIL = 'no.sql@qq.com'
 export const SITE_NAME = 'astro.yufengbiji.com';
 export const SITE_URL = "https://astro.yufengbiji.com";
+
+export const FOOTER_ITEMS = {
+    PRODUCTS: [
+        {
+            name: '本站',
+            url: '/'
+        },
+        {
+            name: 'TOP Link',
+            url: 'https://the.top'
+        },
+        {
+            name: 'TOP News',
+            url: 'https://news.the.top'
+        }
+    ],
+    COMMUNITIES: [
+        {
+            name: 'Twitter',
+            url: 'https://twitter.com/austinit'
+        },
+        {
+            name: 'Github',
+            url: 'https://github.com/austin2035'
+        },
+        {
+            name: 'Telegram',
+            url: 'https://t.me/austin2035'
+        }
+    ],
+    RESOURCES: [
+        {
+            name: 'THE.TOP',
+            url: 'https://the.top'
+        }
+    ]
+}
