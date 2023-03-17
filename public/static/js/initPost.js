9b9f29acc085ce72a94984f48d49d63a1dc81fb5
Lance Liao <llshu2003@gmail.com>
2023-03-18 02:29:05 +0800

    One more fix to handle absolute image path

--- public/static/js/initPost.js
+++ public/static/js/initPost.js
@@ -48,23 +48,24 @@ function initLazyLoad() {
 
       let sign = md5(grandSon.src);
 
+      target = document.getElementById(`lht${sign}`)
+      if (!target)  {
+          // If an absolute path is used as the image link, such as "/static/img.png",
+          // the URL of grandSon.src will become "https://example.com/static/img.png", resulting in a different md5.
+          // Therefore, we attempt to handle this situation by trying again with the absolute path.
+          const a = document.createElement('a');
+          a.href = grandSon.src;
+          sign = md5(a.pathname);
+      }
+
       img.onload = function () {
         let percent = ((img.height / img.width) * 100).toFixed(5);
         var style = document.createElement("style");
         style.innerHTML = renderStyle(sign, percent);
         let target = document.getElementById(`lht${sign}`)
 
-        if (!target) {
-          // If an absolute path is used as the image link, such as "/static/img.png",
-          // the URL of grandSon.src will become "https://example.com/static/img.png", resulting in a different md5.
-          // Therefore, we attempt to handle this situation by trying again with the absolute path.
-          const a = document.createElement('a');
-          a.href = grandSon.src;
-          let sign = md5(a.pathname);
+        if (!target) return;
 
-          target = document.getElementById(`lht${sign}`)
-          if (!target) return;
-        };
         target.parentNode.insertBefore(style, target);
         item.classList.remove("image-load");
         item.classList.add("image-loaded");
